6f200d8419a085befb88386b650b81ffca1280cc
Eleonor Sana <eleonorsana97@gmail.com>
2021-05-12 11:39:27 +0200

    Updates

--- Hospython.py
+++ Hospython.py
@@ -1,4 +1,7 @@
-BeginChoice = int(input("AH-AH DOMMAGE POUR TOI! Tu es coincé dans l'unité infecté par le virus Covid, ta mission, si tu l'accesptes (sinon tape \"5\" pour quitter le jeu), est de sorir vivant de l'unité Covid en X minutes.Tu peux essayer de quitter l'unité en prenant 3  chemis différents, mais attention, ce ne sera pas si simple, des épreuves t'y attendent...tape \"1\" pour aller a gauche, \"2\" pour aller en avant et \"3\" pour aller a droite et \"0\" pour reculer"))
+from random import *
+import time
+
+BeginChoice = int(input("AH-AH DOMMAGE POUR TOI!\n Tu es coincé dans l'unité infecté par le virus Covid, ta mission, si tu l'accesptes (sinon tape \"5\" pour quitter le jeu), est de sortir vivant de l'unité Covid en 90 secondes. Tu peux essayer de quitter l'unité en prenant 3 chemins différents, mais attention, ce ne sera pas si simple, des épreuves t'y attendent...tape \"1\" pour aller a gauche, \"2\" pour aller en avant et \"3\" pour aller a droite et \"0\" pour reculer. \n A toi de jouer! \n"))
 if BeginChoice==5:
     print("Vous n'avez pas le courage de combattre le virus")
 if BeginChoice==1:
--- isaac.py
+++ isaac.py
@@ -1,28 +1,37 @@
-from random import *
+
 numberTry=5
 numberSmall=0
-numberBig=100
-enigmaChoice = randint(numberSmall, numberBig)
+nomberBig=100
+enigmaChoice=randint(nomberSmall, nomberBig)
 addingTime=15
 addingPv=2
 
-print("GENIAL! De la compagnie, je n'ai vu personne depuis que je susi malade (13 mars 2020) Tu resteras avec moi temps que tu n'auras pas résolu mon enigme, mais attention, car si tu restes trop longtemps, tu seras contaminé!\n Voici l'énigme:\n Je pense a un chiffre entre" , numberSmall , "et" , numberBig , "tu as le droit à " , numberTry , "propositions, en fonction des tes réponses je te donnerai un indice.")
+print("GENIAL! De la compagnie, je n'ai vu personne depuis que je susi malade (13 mars 2020) Tu resteras avec moi temps que tu n'auras pas résolu mon enigme, mais attention, car si tu restes trop longtemps, tu seras contaminé!\n
+	Voici l'énigme:\n
+	Je pense a un chiffre entre" , nomberSmall , "et" , numberBig , "tu as le droit à " , numberTry , "propositions, en fonction des tes réponses je te donnerai un indice."
 
-# global beginTime
-# global beginPv 	
+global beginTime
+global beginPv 	
 
-i=1
-playerChoice = int(input("Donne ton choix numéro " + str(i) + " : "))
+
+
+
+i = 1
+playerChoice = input("Donne ton choix numéro " + str(numberTry) + " : ")
 while i < 5 and playerChoice != enigmaChoice: 
     if(playerChoice < enigmaChoice):
         # numberSmall = playerChoice
-        remainChoice = numberTry - i 
-        playerChoice = int(input("plus haut! il te reste " + str(remainChoice) + " choix, quel chiffre choisis tu ?:"))
+        remainChoice = numberChoice - i 
+        playerChoice = input("plus haut! il te reste " + str(ramainChoice) + " choix, quel chiffre choisis tu ?:")
     elif playerChoice > enigmaChoice: 
-        playerChoice = int(input("plus bas! il te reste " + str(remainChoice) + " choix, quel chiffre choisis tu ?:"))
-    i +=1
+        playerChoice = input("plus bas! il te reste " + str(ramainChoice) + " choix, quel chiffre choisis tu ?:")
 else: 
     if playerChoice == enigmaChoice: 
         print("OOOOOh dommage, tu vas devoir quitter ma chambre. \n Comme récompense, je t'offre" + str(addingTime) + " secondes ou pv. \n courage pour trouver ton chemin....")
     else:
         print("Super tu as été infecté, On va partager ma chambre pour la vie ") 
+
+-
+
+
+-
