b089773c62ae940db036276b00fe04df8c950629
Mahsum Kizmaz <mahsumkizmaz@gmail.com>
2021-05-12 10:36:02 +0200

    jeu enigme

---
+++ mahsum.py
@@ -0,0 +1,14 @@
+playerChoice=int(input(" "))
+nomberTry=5
+nomberSmall=0
+nomberBig=100
+enigmaChoice=randint(nomberSmall, nomberBig)
+addingTime=15
+addingPv=2
+
+print("GENIAL! De la compagnie, je n'ai vu personne depuis que je susi malade (13 mars 2020) Tu resteras avec moi temps que tu n'auras pas résolu mon enigme, mais attention, car si tu restes trop longtemps, tu seras contaminé!\n
+	Voici l'énigme:\n
+	Je pense a un chiffre entre" , nomberSmall , "et" , nomberBig , "tu as le droit à " , nomberTry , "propositions, en fonction des tes réponses je te donnerai un indice."
+
+global beginTime
+global beginPv 	
