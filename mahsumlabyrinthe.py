4b0b600c82b95817e7e7287621a11c27cb6933ff
Mahsum Kizmaz <mahsumkizmaz@gmail.com>
2021-05-12 14:21:46 +0200

    jeu labyrinthe

---
+++ mahsumlabyrinthe.py
@@ -0,0 +1,19 @@
+playerChoice = int(input(" "))
+left = 1
+front = 2 
+right = 3
+goBack = 0 
+
+if playerChoice == 1:
+	print("te voilà dans un couloir et 3 choix s'offre à toi.")
+	if playerChoice == 2:
+	print("te voilà dans un couloir et 3 choix s'offre à toi.")
+	if playerChoice == 1:
+		print("Félicitation, tu t'es échapé du Donjon, et tu es vivant!!!! 
+	
+	if playerChoice == 2:
+		print("tu es dans une chambre de contaminé! sort vite en tappant 0")
+		if playerChoice == 3:
+			print("tu es dans une chambre de contaminé! sort vite en tappant 0")
+		if playerChoice == 0:
+			print("tu es revenu à l'endroit ou tu te trouvai. quel chois vas tu prendre?")
