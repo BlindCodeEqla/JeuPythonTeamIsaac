8881a96d09b95535c240b20c71497d6e1ac998de
zackakyo <tcheuyassiisaac@yahoo.com>
2021-05-12 11:41:48 +0200

    test

--- isaac.py
+++ isaac.py
@@ -7,7 +7,7 @@ addingTime=15
 addingPv=2
 
 print("GENIAL! De la compagnie, je n'ai vu personne depuis que je susi malade (13 mars 2020) Tu resteras avec moi temps que tu n'auras pas résolu mon enigme, mais attention, car si tu restes trop longtemps, tu seras contaminé!\n Voici l'énigme:\n Je pense a un chiffre entre" , numberSmall , "et" , numberBig , "tu as le droit à " , numberTry , "propositions, en fonction des tes réponses je te donnerai un indice.")
-
+  
 # global beginTime
 # global beginPv 	
 
